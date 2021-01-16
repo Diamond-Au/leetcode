6cb1759032abc27a3cf9426b9cb5bb7c2c7e9b1e
Diamond-Au <497350746@qq.com>
2021-01-16 15:38:22 +0800

    feat: fix error

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 const int N = 1e6 + 10;
 int n;
@@ -7,21 +8,18 @@ int q[N], tem[N];
 void merge_sort(int l, int r)
 {
   if (l >= r)
-  {
     return;
-  }
-
-  int mid = (l + r) >> 1, i = l, j = mid + 1;
-  int k = 0;
+  int mid = (l + r) >> 1;
 
   merge_sort(l, mid);
   merge_sort(mid + 1, r);
-
+  int i = l, j = mid + 1, k = 0;
   while (i <= mid && j <= r)
   {
     if (q[i] <= q[j])
       tem[k++] = q[i++];
-    tem[k++] = q[j++];
+    else
+      tem[k++] = q[j++];
   }
 
   while (i <= mid)
@@ -29,24 +27,18 @@ void merge_sort(int l, int r)
   while (j <= r)
     tem[k++] = q[j++];
 
-  for (i = l, j = 0; i < r; i++, j++)
-  {
+  for (i = l, j = 0; i <= r; i++, j++)
     q[i] = tem[j];
-  }
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
   scanf("%d", &n);
   for (int i = 0; i < n; i++)
-  {
     scanf("%d", &q[i]);
-  }
-  merge_sort(0, n - 1);
 
+  merge_sort(0, n - 1);
   for (int i = 0; i < n; i++)
-  {
-    printf("%d", q[i]);
-  }
+    printf("%d ", q[i]);
   return 0;
 }
--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
+
 using namespace std;
 const int N = 1e6 + 10;
-
 int n;
 int q[N];
 
-void quick_sort(int l, int r)
+void quick_sork(int l, int r)
 {
   if (l >= r)
-  {
     return;
-  }
 
-  int x = q[(l + r) >> 1], i = l - 1, j = r + 1;
-  while (l < r)
+  int x = q[(l + r) / 2], i = l - 1, j = r + 1;
+
+  while (i < j)
   {
     do
       i++;
@@ -23,13 +22,11 @@ void quick_sort(int l, int r)
     while (q[j] > x);
     if (i < j)
     {
-      int tem = q[i];
-      q[i] = q[j];
-      q[j] = tem;
+      swap(q[i], q[j]);
     }
   }
-  quick_sort(l, j);
-  quick_sort(j + 1, r);
+  quick_sork(l, j);
+  quick_sork(j + 1, r);
 }
 
 int main()
@@ -39,15 +36,13 @@ int main()
   {
     scanf("%d", &q[i]);
   }
+
+  quick_sork(0, n - 1);
+
   for (int i = 0; i < n; i++)
   {
-    printf("%d", q[i]);
+    printf("%d ", q[i]);
   }
-  getchar();
-  quick_sort(0, n - 1);
-  for (int i = 0; i < n; i++)
-  {
-    printf("%d", q[i]);
-  }
+
   return 0;
 }
