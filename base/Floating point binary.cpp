3298ee62bfa68d42928df48177588728a882c52c
Diamond-Au <497350746@qq.com>
2021-01-21 12:21:18 +0800

    fix: bug

--- base/Floating point binary.cpp
+++ base/Floating point binary.cpp
@@ -7,8 +7,10 @@ int main(int argc, char const *argv[])
 {
   double x;
   cin >> x;
-
-  double l = 0, r = x;
+  /*
+    如果 x = 0.01 的话 开根号的话是0.01 不在答案的范围内 应该去 r = max(1, x);
+   */
+  double l = 0, r = x >= 1 ? x : 1;
   while (r - l > 1e-8)
   {
     double mid = (l + r) / 2;
